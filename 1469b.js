07ae847b976ff799698b6daaf5bac3e6a0c748ca
davidpharrington <davidpharrington@gmail.com>
2020-07-27 09:30:24 -0600

    7-27-2020

--- 1469b.js
+++ 1469b.js
@@ -4,30 +4,34 @@ function TreeNode(val, left, right) {
     this.right = (right === undefined ? null : right)
 }
 
-t1 = new TreeNode(1)
-t2 = new TreeNode(2)
-t3 = new TreeNode(3)
-t4 = new TreeNode(4)
-t5 = new TreeNode(5)
-t6 = new TreeNode(6)
-t7 = new TreeNode(7)
-t8 = new TreeNode(8)
-t9 = new TreeNode(9)
+t1 = new TreeNode(99)
+t2 = new TreeNode(99)
+t3 = new TreeNode(77)
+t4 = new TreeNode(55)
+t5 = new TreeNode(33)
+
+t6 = new TreeNode(88)
+t7 = new TreeNode(66)
+t8 = new TreeNode(44)
+t9 = new TreeNode(22)
 
 t1.left = t2
-t1.right = t3
-t2.left = t4
-t2.right = t5
-t4.left = t6
-t4.right = t7
-t6.left = t8
-t3.left = t9
+t2.left = t3
+t3.left = t4
+t4.left = t5
 
+t1.right = t6
+t6.right = t7
+t7.right = t8
+t8.right = t9
 
 var getLonelyNodes = function (root) {
     var n = []
-    console.log(root.val, root.right, root.left)
+    if (!root.right && root.left)  n.push(...[root.left.val])
+    if (root.right && !root.left)  n.push(...[root.right.val])
+    if (root.right) n.push(...getLonelyNodes(root.right))
+    if (root.left) n.push(...getLonelyNodes(root.left))
     return n
 }
 
-getLonelyNodes(t1)
+console.log(getLonelyNodes(t1))
